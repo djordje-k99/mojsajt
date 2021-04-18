869270942745a6cbb9e90d24feaac7d32c01ff1c
djordje-k99 <82780664+djordje-k99@users.noreply.github.com>
2021-04-18 23:20:03 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>PHP primeri</h1>
+    <a href="PHP0.php">0</a>
+    <a href="PHPklase.php">1</a>
+    <a href="PHPniz.php">2</a>
+    <a href="PHPwhile.php">3</a>
+    <a href="PHPxy.php">4</a>
+</body>
+</html>
